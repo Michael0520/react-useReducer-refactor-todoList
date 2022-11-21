48cd6ffdd208219c0ff826bb468ac16c849d261a
Michael0520 <po2319987@gmail.com>
2022-11-22 02:44:10 +0800

    update: use ACTIONS and create default_todoObj

--- src/context/TodoContext.jsx
+++ src/context/TodoContext.jsx
@@ -1,8 +1,35 @@
 import { createContext, useReducer } from "react";
-import TodoReducer, { initState } from "../components/TodoReducer";
+import TodoReducer, { initState, ACTIONS } from "../components/TodoReducer";
 
 export const TodoContext = createContext(initState);
 
 export const TodoProvider = ({ children }) => {
   const [state, dispatch] = useReducer(TodoReducer, initState);
+
+  const addTodo = (todoContent) => {
+    const todoData = todoObj(todoContent);
+    const newTodo = state.todoList.concat(todoData);
+
+    dispatch({
+      type: ACTIONS.ADD_TODO,
+      payload: {
+        todo: newTodo,
+      },
+    });
+  };
+
+  const value = {
+    todoList: state.todoList,
+    addTodo,
+  };
+
+  return <TodoContext.Provider value={value}>{children}</TodoContext.Provider>;
+};
+
+export const todoObj = (todoContent) => {
+  return {
+    id: Math.floor(Math.random() * 10000), // 先用這個方式解決對應 id
+    todoContent,
+    complete: false,
+  };
 };
